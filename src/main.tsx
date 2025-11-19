e5ed784ae06995e2b773e38c4e0d9eb6eb07b0a9
Yoko <yokofurukawa_456@berkeley.edu>
2025-11-19 10:57:36 -0800

    added about

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./app/App.tsx";
+import About from "./app/about/About.tsx";
+import { BrowserRouter, Route, Routes } from "react-router"; 
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<App /> }/> 
+    <Route path="/about" element={<About /> }/> 
+    </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
